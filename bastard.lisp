8599481de32ae1a7339365a11330e0c9d464e4a3
Alex Recker <alex@reckerfamily.com>
2019-07-29 23:29:46 -0500

    symlinks!

--- bastard.lisp
+++ bastard.lisp
@@ -1,6 +1,6 @@
 (defpackage bastard
   (:use :cl)
-  (:export :main :hardlink))
+  (:export :main :join :each :hardlink :symlink))
 
 (require :sb-posix)
 (require :uiop)
@@ -41,8 +41,10 @@ stderr."
 (defun hardlinked-p (src dest)
   "Returns T if pathspec SRC and DEST are already hardlinked to one
 another."
+  (let ((src (abspath src))
+	(dest (abspath dest)))
   (equal (sb-posix:stat-ino (sb-posix:stat src))
-	 (sb-posix:stat-ino (sb-posix:stat dest))))
+	 (sb-posix:stat-ino (sb-posix:stat dest)))))
 
 (defun hardlink (src dest)
   "Creates a hardlink from pathspec SRC to DEST.  
@@ -62,6 +64,44 @@ Overwrites DEST if already linking to another file."
 	   (sb-posix:link src dest)
 	   (report symbol :changed info-msg))
 	  (t (report symbol :up-to-date info-msg)))))
+
+(defun each (pattern)
+  "Return a list of files matching a relative path, wildcard
+expression."
+  (uiop:directory-files (abspath pattern)))
+
+(defun join (&rest paths)
+  "Join directory names."
+  (apply #'merge-pathnames paths))
+
+(defun symlinked-p (src dest)
+  "Return T if SRC is already symlinked to DEST."
+  (let ((src (abspath src))
+	(dest (abspath dest)))
+    (handler-case (equal src (abspath (sb-posix:readlink dest)))
+      (sb-posix:syscall-error (e)
+	(declare (ignore e))
+	nil))))
+
+(defun symlink (src dest)
+  "Create a symlink from SRC to DEST.  Delete DEST if it's already
+there, whether it's a file or another symlink."
+  (let* ((src (abspath src))
+	 (dest (abspath dest))
+	 (info-msg (format nil "~A -> ~A"  src dest))
+	 (symbol "SL"))
+    (cond ((not (probe-file src))
+	   (let ((error-msg (format nil "~A does not exist" src)))
+	     (report symbol :error info-msg error-msg)))
+	  ((not (probe-file dest))
+	   (sb-posix:symlink src dest)
+	   (report symbol :changed info-msg))
+	  ((and (probe-file dest) (symlinked-p src dest))
+	   (report symbol :up-to-date info-msg))
+	  ((probe-file dest)
+	   (delete-file dest)
+	   (sb-posix:symlink src dest)
+	   (report symbol :changed info-msg)))))
 
 (defun collect-forms (filename)
   "Collect all the forms from a lisp file."
--- example.lisp
+++ example.lisp
@@ -3,3 +3,6 @@
 (bastard:hardlink "configs/gitconfig" "~/.gitconfig")
 (bastard:hardlink "configs/gitconfig-work" "~/.gitconfig-work")
 (bastard:hardlink "configs/ssh_config" "~/.ssh/config")
+
+(dolist (script (bastard:each "scripts/*.sh"))
+  (bastard:symlink script (bastard:join "~/bin/" script)))

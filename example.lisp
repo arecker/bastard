d806122cf0875ee0d5eb2a269d738a4ec2e94e6b
Alex Recker <alex@reckerfamily.com>
2019-07-30 08:53:57 -0500

    changed

--- example.lisp
+++ example.lisp
@@ -1,8 +1,20 @@
-(bastard:hardlink "configs/sbclrc.lisp" "~/.sbclrc")
-(bastard:hardlink "configs/gemrc" "~/.gemrc")
-(bastard:hardlink "configs/gitconfig" "~/.gitconfig")
-(bastard:hardlink "configs/gitconfig-work" "~/.gitconfig-work")
-(bastard:hardlink "configs/ssh_config" "~/.ssh/config")
+;; git
+(bastard:symlink "configs/git/gitconfig" "~/.gitconfig")
+(bastard:symlink "configs/git/gitconfig-work" "~/.gitconfig-work")
 
+;; ssh
+(bastard:symlink "configs/ssh/id_rsa.pub" "~/.ssh/id_rsa.pub")
+(bastard:symlink "configs/ssh/config" "~/.ssh/config")
+
+;; ansible
+(bastard:symlink "configs/ansible/ansible.cfg" "~/.ansible.cfg")
+
+;; lisp
+(bastard:symlink "configs/lisp/sbclrc.lisp" "~/.sbclrc")
+
+;; ruby
+(bastard:symlink "configs/ruby/gemrc" "~/.gemrc")
+
+;; scripts
 (dolist (script (bastard:each "scripts/*.sh"))
   (bastard:symlink script (bastard:join "~/bin/" script)))
